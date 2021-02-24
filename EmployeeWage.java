cb8ef5ee7cdf622b1a6bf6d510e81951573014a8
RachanaBR <brrachanajain@gmail.com>
2021-02-24 05:49:07 +0530

    added attendance of Employee to Progarm

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,2 +1,32 @@
 //Welcome to EmployeeWageProgram
+abstract class EmployeeWage1 {
+    abstract void attendance(); 	
+}
+
+class EmployeeCheck extends EmployeeWage
+{
+	public static final int IS_FULL_TIME=1;
+	public void attendance() {
+		
+	double empCheck=Math.floor(Math.random()*10)%2;
+	if(empCheck== IS_FULL_TIME )
+		System.out.println("Employee is Present");
+	else
+		System.out.println("Employee is Absent");	
+     }
+	
+}
+
+class EmployeeWage
+{
+	public static void main(String[] args) {
+		EmployeeCheck employee=new EmployeeCheck();
+		employee.attendance();
+	}
+}
+
+
+
+
+
 
